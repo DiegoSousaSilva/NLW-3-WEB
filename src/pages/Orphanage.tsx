9124ebe2f5c7207bb819852b918f88ac23596c80
Diego Sousa <derickbenji2@gmail.com>
2021-01-18 22:51:47 -0300

    finalizando pagina de detalhe

--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -1,16 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { FaWhatsapp } from "react-icons/fa";
-import { FiClock } from "react-icons/fi";
+import { FiClock, FiInfo } from "react-icons/fi";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import {useParams} from 'react-router-dom';
 
 import mapIcon from "../utils/mapIcon";
 
 import '../styles/pages/orphanage.css';
 import Sidebar from "../components/Sidebar";
 import { Link } from "react-router-dom";
+import api from "../services/api";
 
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+  about: string;
+  instructions: string;
+  opening_hours: string;
+  open_on_weekends: string;
+  images: Array<{
+    id: number;
+    url: string;
+  }>;
+}
+
+interface OrphanageParams {
+  id: string;
+}
 
 export default function Orphanage() {
+  const params = useParams<OrphanageParams>();
+  const [orphanage, setOrphanage] = useState<Orphanage>();
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
+  useEffect(()=>{
+    api.get(`orphanages/${params.id}`)
+      .then(res => {
+        setOrphanage(res.data);
+      });
+
+  }, [params.id]);
+
+  if(!orphanage){
+    return <p>Carregando...</p>
+  }
   return (
 
     <div id="page-orphanage">
@@ -18,35 +53,35 @@ export default function Orphanage() {
 
       <main>
         <div className="orphanage-details">
-          <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das crianças" />
+          <img src={orphanage.images[activeImageIndex].url} alt={orphanage.name} />
 
           <div className="images">
-
-            <button className="active" type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das crianças"/>
-            </button>
-
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das crianças"/>
-            </button>
-           
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das crianças"/>
-            </button>
-
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das crianças"/>
-            </button>
+          {
+            orphanage.images.map((image, index) =>{
+              return(
+                <button 
+                  key={image.id} 
+                  className={activeImageIndex === index ? "active" : ""} 
+                  type="button"
+                  onClick={()=>{
+                    setActiveImageIndex(index)
+                  }}
+                >
+                  <img src={image.url} alt={orphanage.name}/>
+                </button>
+              )
+            })
+          }
             
           </div>
 
           <div className="orphanage-details-content">
-            <h1>Lar das Crianças</h1>
-            <p>Prestar assistência a crianças de 06 a 15 anos que se encontra em situação de risco e/ou vulnerabilidade</p>
+            <h1>{orphanage.name}</h1>
+            <p>{orphanage.about}</p>
 
             <div className="map-container">
               <MapContainer
-                center={[-5.5132955,-47.4645257]}
+                center={[orphanage.latitude,orphanage.longitude]}
                 zoom={16}
                 style={{ width: '100%', height: 280 }}
                 dragging={false}
@@ -55,28 +90,42 @@ export default function Orphanage() {
                 scrollWheelZoom={false}
                 doubleClickZoom={false}
               >
-                <TileLayer
-                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-                />
-                <Marker interactive={false} icon={mapIcon} position={[-5.5132955,-47.4645257]} />
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> 
+               {/* <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>*/}
+                <Marker interactive={false} icon={mapIcon} position={[orphanage.latitude,orphanage.longitude]} />
               </MapContainer>
 
               <footer>
-                <Link to="#">Ver rotas no Google Maps</Link>
+                <a target="_blank" rel="noopener noreferrer" href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude}, ${orphanage.longitude}`}>Ver rotas no Google Maps</a>
               </footer>
             </div>
 
             <hr />
 
             <h2>Instruções para visita</h2>
-            <p>Instrucoes</p>
+            <p>{orphanage.instructions}</p>
 
             <div className="open-details">
               <div className="hour">
                 <FiClock size={32} color="#15B6D6" />
                 Segunda à Sexta <br />
-                Aberto
+                {orphanage.opening_hours}
               </div>  
+
+              {orphanage.open_on_weekends? (
+                <div className="open-on-weekends">
+                  <FiInfo size={32} color="#39CC83"/>
+                  Atendemos<br/>
+                  fim de semana
+                </div>
+              )
+              :(
+                <div className="open-on-weekends-dont-open">
+                  <FiInfo size={32} color="#FF6690"/>
+                  Não atendemos<br/>
+                  fim de semana
+                </div>
+              )}
 
             </div>
  .
